ac5767a041df70bcd0c096b7e9eba4ef1e513fad
fel111 <felix.goupil@free.fr>
2018-03-13 17:36:48 +0100

    finalisation modele compact lotsiz et lecteur fichier pwd

--- Lot Sizing/Lot Sizing Compact/lotsizingcom.cpp
+++ Lot Sizing/Lot Sizing Compact/lotsizingcom.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <limits>
 #include "scip/scip.h"
 #include "scip/scipdefplugins.h"
 #include <ilcplex/cplex.h>
 using namespace std;
 
+float infini = numeric_limits<float>::infinity();
 
 
-void lotSizComp(int cardT, int qmax, vector<int> dt, vector<vector<float> > p_ki){
+float p_t(int t,vector<int> nb_bp,vector<vector <int> > bpt,vector<vector <int> > valbpt,vector<vector <float> > pente, float x){
+	if (x==0.0) return 0.0;	
+	for (int i=0; i<nb_bp[t]; ++i){
+		if((x>bpt[t][i])&&(x<=bpt[t][i+1])) 
+			return pente[t][i]*x+valbpt[t][i];
+	}
+}
+
+
+void lotSizComp(int cardT, int qmax, vector<int> dt, vector<vector<int> > bpt, vector<vector<int> > valbpt, vector<vector <float> > pente,vector<int> nb_bp){
 	SCIP * scip;
 	SCIPcreate(&scip);
 	SCIPincludeDefaultPlugins(scip);
-	SCIPcreateProb(scip, "LN", 0, 0, 0, 0, 0, 0, 0);
+	SCIPcreateProb(scip, "LotSizComp", 0, 0, 0, 0, 0, 0, 0);
+
 
 	//VARIABLES
 	//ajout variables ct
@@ -24,11 +36,11 @@ void lotSizComp(int cardT, int qmax, vector<int> dt, vector<vector<float> > p_ki
 	}
 
 	//ajout variables rt
-	vector<SCIP_VAR *> rt;
+	vector<SCIP_VAR *> xt;
 	for(int i=0; i<cardT; ++i){
 		SCIP_VAR * var;
-		rt.push_back(var);
-		SCIPcreateVarBasic(scip, &rt[i], ("rt"+to_string(i)).c_str(), -qmax, +qmax, 0, SCIP_VARTYPE_CONTINUOUS);
+		xt.push_back(var);
+		SCIPcreateVarBasic(scip, &xt[i], ("xt"+to_string(i)).c_str(), 0, SCIP_REAL_MAX, 0, SCIP_VARTYPE_CONTINUOUS);
 	}
 	
 	//ajout variables st de stock
@@ -46,13 +58,13 @@ void lotSizComp(int cardT, int qmax, vector<int> dt, vector<vector<float> > p_ki
 	for(int i=0; i<cardT; ++i){
 		SCIP_CONS * cons;
 		cons_ct.push_back(cons);
-		SCIPcreateConsLinear(scip, &cons_ct[i], ("cons_ct"+to_string(i)).c_str(), 0, 0, 0, p_ki[i][dt[i]], p_ki[i][dt[i]], TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE);  
+		SCIPcreateConsLinear(scip, &cons_ct[i], ("cons_ct"+to_string(i)).c_str(), 0, 0, 0, p_t(i,nb_bp,bpt,valbpt,pente,xt[i]), p_t(i,nb_bp,bpt,valbpt,pente,xt[i]), TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE);  
 		SCIPaddCoefLinear(scip, cons_ct[i], ct[i],1);
 		SCIPaddCons(scip, cons_ct[i]);
 	}
 	
 	//contraintes bilan energetique
-	vector<SCIP_CONS *> cons_dt;
+	/*vector<SCIP_CONS *> cons_dt;
 	for(int i=1; i<cardT; ++i){
 		SCIP_CONS * cons;
 		cons_dt.push_back(cons);
@@ -61,16 +73,16 @@ void lotSizComp(int cardT, int qmax, vector<int> dt, vector<vector<float> > p_ki
 		SCIPaddCoefLinear(scip, cons_dt[i], st[i-1],-1);
 		SCIPaddCoefLinear(scip, cons_dt[i], rt[i],1);		
 		SCIPaddCons(scip, cons_dt[i]);
-	}
+	}*/
 
 	//contrainte stmax = 0
-	SCIP_CONS * cons_stmax;
+	/*SCIP_CONS * cons_stmax;
 	SCIPcreateConsLinear(scip,&cons_stmax,"cons_qtmax",0,0,0,0,0,TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE);  	
 	SCIPaddCoefLinear(scip,cons_stmax,st[cardT-1],1);
-	SCIPaddCons(scip,cons_stmax);
+	SCIPaddCons(scip,cons_stmax);*/
 
 	//contrainte evolution stock
-	vector<SCIP_CONS *> cons_st;
+	/*vector<SCIP_CONS *> cons_st;
 	for(int i=0; i<cardT-1; ++i){
 		SCIP_CONS * cons;
 		cons_st.push_back(cons);
@@ -79,7 +91,7 @@ void lotSizComp(int cardT, int qmax, vector<int> dt, vector<vector<float> > p_ki
 		SCIPaddCoefLinear(scip, cons_st[i], st[i],-1);
 		SCIPaddCoefLinear(scip, cons_st[i], rt[i],1);		
 		SCIPaddCons(scip, cons_st[i]);
-	}
+	}*/
 
 
 	//RESOLUTION
@@ -88,7 +100,7 @@ void lotSizComp(int cardT, int qmax, vector<int> dt, vector<vector<float> > p_ki
 	//RECUPERATION
 	SCIP_SOL * sol = SCIPgetBestSol(scip);
 	cout << "obj = " << SCIPgetSolOrigObj(scip,sol) << endl << endl;
-	cout << "etat de la charge :" << endl;	
+	/*cout << "etat de la charge :" << endl;	
 	for(int i=0; i<cardT; ++i){
 		cout << "s"<<i<<" = "<<SCIPgetSolVal(scip,sol,st[i]) << endl;
 	}
@@ -96,22 +108,29 @@ void lotSizComp(int cardT, int qmax, vector<int> dt, vector<vector<float> > p_ki
 	cout << "Cout energie : "<<endl;
 	for(int i=0; i<cardT; ++i){
 		cout << "ct"<<i<<" = "<<SCIPgetSolVal(scip,sol,ct[i]) << endl;
-	}
+	}*/
 	
 
 }
 
 int main(){
 	int cardT = 5; 
-	int q_max = 100;
+	int qmax = 100;
 	vector<int> dt = {5,10,15,20,25};
-	vector<vector<float> > p_ki;
+	vector<vector<int> > bpt;
+	vector<vector<int> > valbpt;
+	vector<vector<float> > pente;
+	vector<int> nb_bp = {3,3,3,3,3};
 	for(int i=0;i<cardT;++i){
-		vector<float> temp = {20,20,15,30,25};
-		p_ki.push_back(temp);
+		vector<float> temp_pente = {1.0,2.0,infini};
+		vector<int> temp_bpt = {0,100,200};
+		vector<int> temp_valbpt = {0,100,300};
+		pente.push_back(temp_pente);
+		bpt.push_back(temp_bpt);
+		valbpt.push_back(temp_valbpt);
 	}
 
-	lotSizComp(cardT,q_max,dt,p_ki);
+	lotSizComp(cardT,qmax,dt,bpt,valbpt,pente,nb_bp);
 
 	return 0;
 }
